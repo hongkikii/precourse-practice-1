362739637da0df9fafe34f1f30517c4663761171
mimi <110226866+hongkikii@users.noreply.github.com>
2024-12-10 16:38:47 +0900

    refactor(Separator): 구분자 검증 메서드 분리

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -2,6 +2,7 @@ package calculator;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class Separator {
     private final Set<Character> separators;
@@ -12,12 +13,28 @@ public class Separator {
         separators.add(':');
 
         if (input.startsWith("//")) {
-            int customSeparatorIdx = input.indexOf("//") + 2;
-            separators.add(input.charAt(customSeparatorIdx));
+            validateCustom(input);
+            separators.add(input.charAt(getCustomStartIdx(input)));
         }
     }
 
     public boolean contains(char c) {
         return separators.contains(c);
+    }
+
+    private void validateCustom(String input) {
+        int start = getCustomStartIdx(input);
+        int end = input.indexOf("\\n") - 1;
+
+        if(end - start != 0) { // 1자리가 아닌 경우
+            throw new IllegalArgumentException();
+        }
+        if(Pattern.matches("^[0-9]+", input.substring(start, end+1))) { // 숫자인 경우
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int getCustomStartIdx(String input) {
+        return input.indexOf("//") + 2;
     }
 }
--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -5,16 +5,7 @@ import java.util.regex.Pattern;
 public class Validator {
     public static void validate(String input) {
         // 커스텀 구분자가 존재하는 경우
-        if(input.startsWith("//") && input.contains("\\n")) {
-            int start = input.indexOf("//") + 2;
-            int end = input.indexOf("\\n") - 1;
-
-            if(end - start != 0) { // 1자리가 아닌 경우
-                throw new IllegalArgumentException();
-            }
-            if(Pattern.matches("^[0-9]+", input.substring(start, end+1))) { // 숫자인 경우
-                throw new IllegalArgumentException();
-            }
+        if(input.startsWith("//") && input.contains("\\n")) { // 커스텀 구분자 정의 규칙을 잘 지키는지
             return;
         }
 
